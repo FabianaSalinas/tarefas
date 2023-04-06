79804544021ee952302018e137e8ad7047bab1bc
fabianasalinas <fabiana@madalogistica.com>
2023-04-06 14:52:28 -0300

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,42 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import Controller from './controllers/Controller';
 
 class App {
 public app: express.Application;
 
-public constructor() {
+public constructor(controllers: Controller[]) {
   this.app = express();
   this.app.use(cors());
+
+  this.initMongoose();
+  this.connectDatabase();
+  this.initExpressJson();
+  this.initControllers(controllers);
+}
+
+private initMongoose(): void {
+  mongoose.set('runValidators', true);
+}
+
+private connectDatabase(): void {
+  mongoose.connect('mongodb+srv://FabianaSalinas:M5vDRTZzgsmMP29T@cluster0.zqkn1sm.mongodb.net/tarefasyarn dev?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+}
+
+private initExpressJson(): void {
+  this.app.use(express.json());
+}
+
+private initControllers(controllers: Controller[]): void {
+  controllers.forEach((controller) => {
+    this.app.use('/', controller.router);
+  });
 }
 
 public listen(port: number): void {
---
+++ src/schemas/User.ts
@@ -0,0 +1,34 @@
+import { model, Schema, Document } from 'mongoose';
+
+export interface UserInterface extends Document {
+    name: string,
+    email: string,
+    password: string,
+    creation: Date,
+}
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'Nome obrigatório'],
+  },
+  email: {
+    type: String,
+    required: [true, 'E-mail Obrigatório'],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Senha obrigatória'],
+    select: false,
+  },
+  creation: {
+    type: Date,
+    default: Date.now,
+
+  },
+});
+
+export default model<UserInterface>('User', UserSchema);
